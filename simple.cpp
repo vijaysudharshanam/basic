293381b8be1362ad5cc092011f780b69e6129b4b
Vijay Kumar Sudharshanam <vijay@gridpro.com>
2014-12-30 12:36:45 +0530

    adding another line

--- simple.cpp
+++ simple.cpp
@@ -1,7 +1,11 @@
 #include <cstdio>
+#include <cmath>
 
 int main()
 {
     printf("Hello world.. Checking jenkins !!\n");
+    int a = 3, b = 4;
+    int c = sqrt(a*a + b*b);
+    printf("py theorem : (%d, %d) -> %d\n", a,b,c);
     return 0;  
 }
